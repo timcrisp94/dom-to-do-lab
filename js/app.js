38b70d928c648c5b95b26bb0cddbde119249816b
Tim Crisp <timothycrisp@gmail.com>
2022-04-24 09:53:10 -0500

    add reset button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 const todoInput = document.getElementById('todo-input')
 const submitBtn = document.getElementById('submit-button')
 const todoList = document.getElementById('todo-list')
+const resetBtn = document.getElementById('reset-button')
 
 submitBtn.addEventListener('click', function(evt) {
-  if (todoInput.value === '') return
+  if (todoInput.value === '') return 
   const newTodo = document.createElement('li')
   newTodo.textContent = todoInput.value
   todoList.appendChild(newTodo)
   todoInput.value = ''
-
 })
+
+resetBtn.addEventListener('click', function(evt) {
+  todoList.innerHTML = ''
+  todoInput.value = ''
+})
+

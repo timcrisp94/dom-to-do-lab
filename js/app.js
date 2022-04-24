f6cfa9c3860f00203e10e431394f2c96c02bcaaf
Tim Crisp <timothycrisp@gmail.com>
2022-04-24 09:48:18 -0500

    add new line to event listener

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ const todoList = document.getElementById('todo-list')
 submitBtn.addEventListener('click', function(evt) {
   if (todoInput.value === '') return
   const newTodo = document.createElement('li')
+  newTodo.textContent = todoInput.value
+  todoList.appendChild(newTodo)
   todoInput.value = ''
 
 })

d9c5506b2e70c746c6e55e29dcccc0eac42592e3
Tim Crisp <timothycrisp@gmail.com>
2022-04-24 09:45:34 -0500

    add event listener to submit btn

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('sanity check')
+const todoInput = document.getElementById('todo-input')
+const submitBtn = document.getElementById('submit-button')
+const todoList = document.getElementById('todo-list')
+
+submitBtn.addEventListener('click', function(evt) {
+  if (todoInput.value === '') return
+  const newTodo = document.createElement('li')
+  todoInput.value = ''
+
+})
